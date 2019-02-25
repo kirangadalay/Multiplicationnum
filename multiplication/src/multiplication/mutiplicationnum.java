0d9219588d13470912261da2048cb9f7d8e4f65e
kirangadalay <gadalaykiran@gmail.com>
2019-02-25 11:02:17 -0500

    multiplicationnum

---
+++ multiplication/src/multiplication/mutiplicationnum.java
@@ -0,0 +1,14 @@
+package multiplication;
+
+public class mutiplicationnum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x, y, z;
+    x = 10;
+    y = 20;
+    z = x * y;
+   System.out.println(z);
+    		}
+
+}
